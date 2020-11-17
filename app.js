ad358712b6fdffa4f6334167f6d3d00cd69a5211
Talon Durrant <talonmdurrant@gmail.com>
2020-11-17 11:03:01 -0700

    connected the mongodb database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
 
 const placesRoutes = require("./routes/places-routes")
 const usersRoutes = require("./routes/users-routes")
@@ -25,4 +26,11 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unknown error occured." })
 })
 
-app.listen(5000)
+mongoose
+  .connect(process.env.DB_CONNECTIONSTRING)
+  .then(() => {
+    app.listen(5000)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
