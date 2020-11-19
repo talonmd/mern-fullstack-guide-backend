91c59500f83f15cdef329ba6be125008bfa325c9
Talon Durrant <talonmdurrant@gmail.com>
2020-11-18 22:59:07 -0700

    adding env variables

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -82,7 +82,7 @@ const signup = async (req, res, next) => {
         userId: createdUser.id,
         email: createdUser.email,
       },
-      "supersecret_dont_share",
+      process.env.JWT_KEY,
       { expiresIn: "1h" }
     )
   } catch (err) {
@@ -131,7 +131,7 @@ const login = async (req, res, next) => {
         userId: existingUser.id,
         email: existingUser.email,
       },
-      "supersecret_dont_share",
+      process.env.JWT_KEY,
       { expiresIn: "1h" }
     )
   } catch (err) {
