dc9d6513dac75d17bb448403cdea970d5c6538ae
Talon Durrant <talonmdurrant@gmail.com>
2020-11-18 21:49:21 -0700

    finished adding authentication and authorization

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -83,7 +83,7 @@ const createPlace = async (req, res, next) => {
     //console.log(errors)
     return next(new HttpError("Invalid inputs passed, please check your data.", 422))
   }
-  const { title, description, address, creator } = req.body
+  const { title, description, address } = req.body
 
   let coordinates
   try {
@@ -98,14 +98,14 @@ const createPlace = async (req, res, next) => {
     address,
     location: coordinates,
     image: req.file.path,
-    creator,
+    creator: req.userData.userId,
   })
 
   //DUMMY_PLACES.push(createdPlace) // unshif(createdPlace) if you want to add it as the first element in the array
 
   let user
   try {
-    user = await User.findById(creator)
+    user = await User.findById(req.userData.userId)
   } catch (err) {
     const error = new HttpError("Creating place failed, please try again.", 500)
     return next(error)
