3268d4ba8e218bff0c9e1747af2119ed47d6d985
Talon Durrant <talonmdurrant@gmail.com>
2020-11-18 23:07:44 -0700

    added env port for heroku

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(process.env.DB_CONNECTIONSTRING)
   .then(() => {
-    app.listen(5000)
+    app.listen(process.env.PORT || 5000)
   })
   .catch((err) => {
     console.log(err)
